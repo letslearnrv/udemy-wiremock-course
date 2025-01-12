d7b1c80887cbee6665ae098dce87ecf0b429b9b8
rahul_verma <rahul_verma@epam.com>
2025-01-12 13:32:35 +0530

    Updated Dependencies

--- src/main/java/Master.java
+++ src/main/java/Master.java
@@ -1,13 +1,9 @@
-import Stubs.*;
-import  com.github.tomakehurst.wiremock.WireMockServer;
-
-
-import  static com.github.tomakehurst.wiremock.client.WireMock.*;
-
-import com.github.tomakehurst.wiremock.client.WireMock;
+import Stubs.FetchUseCases;
+import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.common.ConsoleNotifier;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
-import com.github.tomakehurst.wiremock.extension.responsetemplating.ResponseTemplateTransformer;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.configureFor;
 
 
 
